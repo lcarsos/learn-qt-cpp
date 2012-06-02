1f16d74316aa92ed6ddf9f9b036fd1204c977d1a
lcarsos <lcarsos@gmail.com>
2012-06-02 15:05:02 -0600

    Made a widget, non-resizable

---
+++ proj04/main.cpp
@@ -0,0 +1,29 @@
+#include <QApplication>
+#include <QFont>
+#include <QPushButton>
+#include <QWidget>
+
+class MyWidget : public QWidget
+{
+public:
+    MyWidget( QWidget* parent = 0 );
+};
+
+MyWidget::MyWidget( QWidget* parent ) : QWidget( parent )
+{
+    setFixedSize( 200, 120 );
+    
+    QPushButton* quit = new QPushButton( tr( "Quit" ), this );
+    quit->setGeometry( 62, 40, 75, 30 );
+    quit->setFont( QFont( "Times", 18, QFont::Bold ) );
+    
+    connect( quit, SIGNAL( clicked() ), qApp, SLOT( quit() ) );
+}
+
+int main( int argc, char* argv[] )
+{
+    QApplication app( argc, argv );
+    MyWidget widget;
+    widget.show();
+    return app.exec();
+}
