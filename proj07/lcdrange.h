b7217f24fbdb25e8512b2e20157e7232c20b4f53
lcarsos <lcarsos@gmail.com>
2012-06-14 21:16:15 -0600

    lots of sliders that move together

---
+++ proj07/lcdrange.h
@@ -0,0 +1,26 @@
+#ifndef LCDRANGE_H
+#define LCDRANGE_H
+
+#include <QWidget>
+
+class QSlider;
+
+class LCDRange : public QWidget
+{
+    Q_OBJECT
+        
+public:
+    LCDRange(QWidget* parent = 0);
+
+    int value() const;
+public slots:
+    void setValue(int value);
+
+signals:
+    void valueChanged(int newValue);
+
+private:
+    QSlider* slider;
+};
+
+#endif
